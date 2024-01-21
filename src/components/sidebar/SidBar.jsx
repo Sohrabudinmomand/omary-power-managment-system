55bf1d04147c0b03224b81b531142e27225f86c6
Sohrab momand <sohrabmomand.work@gmail.com>
2024-01-21 18:49:39 +0430

    sidebar updates

--- src/components/sidebar/SidBar.jsx
+++ src/components/sidebar/SidBar.jsx
@@ -9,80 +9,156 @@ import {
   MonetizationOn,
   ShowChart,
   Settings,
+  FiberManualRecord as FiberManualRecordIcon,
 } from "@mui/icons-material";
 
 import SideBarLogo from "./assets/sidebar-logo.png";
 
-const SidBar = () => {
+const Sidebar = () => {
+  const [activeItem, setActiveItem] = useState("Dashboard");
+  const handleItemClick = (item) => {
+    setActiveItem(item);
+  };
+
   const [dropdownMenu, setDropdownMenu] = useState(false);
 
-  const onMouseEnter = () => {
-    setDropdownMenu(true);
+  const onExpenseClick = () => {
+    setDropdownMenu(!dropdownMenu);
   };
-
-  const onMouseLeave = () => {
-    setDropdownMenu(false);
-  };
-
-  const navLinks = [
-    { to: "/", icon: <Home />, label: "Dashboard" },
-    { to: "/Customer", icon: <Person />, label: "Customer" },
-    { to: "/MeterCycle", icon: <Speed />, label: "MeterCycle" },
-    { to: "/Earnings", icon: <ShoppingBasket />, label: "Earnings" },
-    { to: "/HRM", icon: <GroupAdd />, label: "HRM" },
-    { to: "/Expenses", icon: <MonetizationOn />, label: "Expenses" },
-    { to: "/Reports", icon: <ShowChart />, label: "Reports" },
-    { to: "/Settings", icon: <Settings />, label: "Settings" },
-  ];
-
-  const expensesDropdownLinks = [
-    { to: "/expenses/all", label: "All Expenses" },
-    { to: "/expenses/payment-sent", label: "Payment Sent" },
-    { to: "/expenses/payment-received", label: "Payment Received" },
-    { to: "/expenses/owner-pickups", label: "Owner Pickups" },
-    { to: "/expenses/expense-category", label: "Expense Category" },
-    { to: "/expenses/people", label: "People" },
-  ];
 
   return (
     <div className="w-1/5 h-auto flex flex-col px-5 bg-white border relative">
-      <div className="flex items-center justify-center m-2">
+      <div className="flex items-center justify-center">
         <img className="w-[96px] h-[96px]" src={SideBarLogo} alt="" />
       </div>
-      <div className="w-full flex flex-col items-center p-5">
-        {navLinks.map((navLink) => (
-          <Link
-            key={navLink.to}
-            to={navLink.to}
-            className="w-full flex flex-row items-center justify-start mt-4 space-y-2 space-x-6 px-5 py-2 border rounded cursor-pointer"
-          >
-            <span>{navLink.icon}</span>
-            <span className="ml-2">{navLink.label}</span>
-          </Link>
-        ))}
+      <div className="w-full flex flex-col items-center">
+        <Link
+          to="/"
+          className={`w-full flex flex-row items-center justify-start p-4 rounded cursor-pointer
+    ${activeItem === "Dashboard" ? "bg-red-500" : "bg-white"}`}
+          onClick={() => handleItemClick("Dashboard")}
+        >
+          <span>
+            <Home />
+          </span>
+          <span className="ml-2">Dashboard</span>
+        </Link>
+
+        <Link
+          to="/Customer"
+          className={`w-full flex flex-row items-center justify-start p-4 rounded cursor-pointer
+          ${activeItem === "Customers" ? "bg-red-500" : "bg-white"}`}
+          onClick={() => handleItemClick("Customers")}
+        >
+          <span>
+            <Person />
+          </span>
+          <span className="ml-2">Customers</span>
+        </Link>
+
+        <Link
+          to="/MeterCycle"
+          className={`w-full flex flex-row items-center justify-start p-4 rounded cursor-pointer
+          ${activeItem === "Meter Cycle" ? "bg-red-500" : "bg-white"}`}
+          onClick={() => handleItemClick("Meter Cycle")}
+        >
+          <span>
+            <Speed />
+          </span>
+          <span className="ml-2">Meter Cycle</span>
+        </Link>
+
+        <Link
+          to="/Earnings"
+          className={`w-full flex flex-row items-center justify-start p-4 rounded cursor-pointer
+          ${activeItem === "Earnings" ? "bg-red-500" : "bg-white"}`}
+          onClick={() => handleItemClick("Earnings")}
+        >
+          <span>
+            <ShoppingBasket />
+          </span>
+          <span className="ml-2">Earnings</span>
+        </Link>
+
+        <Link
+          to="/HRM"
+          className={`w-full flex flex-row items-center justify-start p-4 rounded cursor-pointer
+          ${activeItem === "HRM" ? "bg-red-500" : "bg-white"}`}
+          onClick={() => handleItemClick("HRM")}
+        >
+          <span>
+            <GroupAdd />
+          </span>
+          <span className="ml-2">HRM</span>
+        </Link>
 
         <div
-          className="w-full flex flex-row items-center justify-start mt-4 space-y-2 space-x-6 px-5 py-2 border rounded cursor-pointer"
-          onMouseEnter={onMouseEnter}
-          onMouseLeave={onMouseLeave}
+          className={`w-full flex flex-row items-center justify-start p-4 rounded cursor-pointer relative
+          ${activeItem === "Expenses" ? "bg-red-500" : "bg-white"}`}
+          onClick={() => handleItemClick("Expenses")}
         >
           <span>
             <MonetizationOn />
           </span>
-          <span className="ml-2">Expenses</span>
+          <span className="ml-2" onClick={onExpenseClick}>
+            Expenses
+          </span>
           {dropdownMenu && (
-            <ul className="absolute left-0 mt-2 bg-white border rounded shadow">
-              {expensesDropdownLinks.map((link) => (
-                <li key={link.to}>
-                  <Link to={link.to}>{link.label}</Link>
-                </li>
-              ))}
-            </ul>
+            <div className="absolute top-10 left-0 mt-2 bg-white border rounded shadow">
+              <Link
+                to="/expenses/all"
+                className={`w-full flex flex-row items-center justify-start p-4 rounded cursor-pointer
+                ${activeItem === "All expenses" ? "bg-red-500" : "bg-white"}`}
+                onClick={() => handleItemClick("All expenses")}
+              >
+                <span>
+                  <FiberManualRecordIcon />
+                </span>
+                <span className="ml-2">All expenses</span>
+              </Link>
+
+              <Link
+                to="/expenses/payment-sent
+              "
+                className={`w-full flex flex-row items-center justify-start p-4 rounded cursor-pointer
+              ${activeItem === "Payment sent" ? "bg-red-500" : "bg-white"}`}
+                onClick={() => handleItemClick("Payment sent")}
+              >
+                <span>
+                  <FiberManualRecordIcon />
+                </span>
+                <span className="ml-2">Payment sent</span>
+              </Link>
+            </div>
           )}
         </div>
+
+        <Link
+          to="/Reports"
+          className={`w-full flex flex-row items-center justify-start p-4 rounded cursor-pointer
+          ${activeItem === "Reports" ? "bg-red-500" : "bg-white"}`}
+          onClick={() => handleItemClick("Reports")}
+        >
+          <span>
+            <ShowChart />
+          </span>
+          <span className="ml-2">Reports</span>
+        </Link>
+
+        <Link
+          to="/Settings"
+          className={`w-full flex flex-row items-center justify-start p-4 rounded cursor-pointer
+          ${activeItem === "Settings" ? "bg-red-500" : "bg-white"}`}
+          onClick={() => handleItemClick("Settings")}
+        >
+          <span>
+            <Settings />
+          </span>
+          <span className="ml-2">Settings</span>
+        </Link>
       </div>
     </div>
   );
 };
 
-export default SidBar;
+export default Sidebar;
